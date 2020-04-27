9156f224f054c18190d7a30686f5b30de04f03f2
King-1025 <1543641386@qq.com>
2020-04-27 16:42:38 +0800

    fix

--- stage/ffmpeg-build.sh
+++ stage/ffmpeg-build.sh
@@ -6,12 +6,6 @@ HOST=aarch64-unknown-linux-android
 
 #${HOST}-populate
 
-
-which $HOST-gcc
-
-$HOST-gcc --help
-echo ""
-
 URL=https://ffmpeg.org/releases/ffmpeg-4.2.2.tar.bz2
 FILE=ffmpeg.tar.bz2
 
@@ -23,7 +17,7 @@ tar -xaf $FILE -C ffmpeg && rm -rf $FILE
 
 cd ffmpeg
 
-CONFIGURE=$(find . -name configure -print)
+CONFIGURE=$(find $(pwd) -name configure -print)
 
 COMMAND=$(cat << EOF
 $CONFIGURE \
