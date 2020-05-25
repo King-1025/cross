de0d267585236591a6c5b086943e21d08f08cc5b
King-1025 <1543641386@qq.com>
2020-05-26 00:17:26 +0800

    添加openssl-build.sh

---
+++ stage/openssl-build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -ex
+
+ROOT=$(pwd)
+RESULT=$ROOT/result
+
+[ ! -e openssl ] && git clone https://github.com/yourname/openssl.git openssl -j 4
+
+cd openssl
+
+./Configure android-arm64 -D__ANDROID_API__=21 --prefix=$RESULT --cross-compile-prefix=aarch64-unknown-linux-android-
+
+make -j 4 && make install
