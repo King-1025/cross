da35cd9567c53b52fce36a284dd588799aa3e7d8
King-1025 <1543641386@qq.com>
2020-05-26 00:34:45 +0800

    fix

--- stage/openssl-build.sh
+++ stage/openssl-build.sh
@@ -9,8 +9,8 @@ RESULT=$ROOT/result
 
 cd openssl
 
-./config --help
+./config -h
 
-./Configure android64-aarch64  -D__ANDROID_API__=21 --prefix=$RESULT --cross-compile-prefix=aarch64-unknown-linux-android-
+#./Configure android64-aarch64  -D__ANDROID_API__=21 --prefix=$RESULT --cross-compile-prefix=aarch64-unknown-linux-android-
 
 make -j 4 && make install
