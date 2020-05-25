0af7b7f8df1bc6c3fcd14a08b2349cf03e2a8578
King-1025 <1543641386@qq.com>
2020-05-25 20:25:05 +0800

    fix

--- stage/ffmpeg-build.sh
+++ stage/ffmpeg-build.sh
@@ -21,7 +21,7 @@ CONFIGURE=$(find $(pwd) -name configure -print)
 
 # sed -i "s/# FFmpeg configure script.*/set -x/g" $CONFIGURE
 
-sed -i "s/B0/b0/g" src/libavcodec/aaccoder.c
+sed -i "s/B0/b0/g" $(find $(pwd) -name "libavcodec/aaccoder.c" -print)
 
 COMMAND=$(cat << EOF
 $CONFIGURE \
