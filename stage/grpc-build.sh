f270a7baaea48dbbdc45b40a4062a6bce3f35ef4
King-1025 <1543641386@qq.com>
2020-05-26 16:26:35 +0800

    make grpc

--- stage/grpc-build.sh
+++ stage/grpc-build.sh
@@ -6,37 +6,77 @@ ROOT=$(pwd)
 GRPC=$ROOT/grpc
 RESULT=$ROOT/result
 
-sudo apt install -y build-essential autoconf libtool pkg-config libssl-dev wget
+function app()
+{
+   get_repo
+   install_cmake
+   cross_compile_grpc
+}
 
-rm -rf $GRPC && mkdir -p $GRPC && git clone --recurse-submodules -b v1.28.1 https://github.com/grpc/grpc $GRPC -j 4 && cd $GRPC
+function get_repo()
+{
+    # sudo apt install -y build-essential autoconf libtool pkg-config libssl-dev wget
 
-# Install CMake 3.16
-wget -q -O cmake-linux.sh https://github.com/Kitware/CMake/releases/download/v3.16.1/cmake-3.16.1-Linux-x86_64.sh
-sudo sh cmake-linux.sh -- --skip-license --prefix=/usr
-rm cmake-linux.sh
+    rm -rf $GRPC && mkdir -p $GRPC && git clone --recurse-submodules -b v1.28.1 https://github.com/grpc/grpc $GRPC -j 4 && cd $GRPC
+}
 
+function install_cmake()
+{
+   # Install CMake 3.16
+   wget -q -O cmake-linux.sh https://github.com/Kitware/CMake/releases/download/v3.16.1/cmake-3.16.1-Linux-x86_64.sh
+   sudo sh cmake-linux.sh -- --skip-license --prefix=/usr
+   rm cmake-linux.sh
+}
+
+function build_host_grpc()
+{
 # Build and install gRPC for the host architecture.
 # We do this because we need to be able to run protoc and grpc_cpp_plugin
 # while cross-compiling.
-#mkdir -p "cmake/build"
-#pushd "cmake/build"
-#cmake \
-#  -DCMAKE_BUILD_TYPE=Release \
-#  -DgRPC_INSTALL=ON \
-#  -DgRPC_BUILD_TESTS=OFF \
-#  -DgRPC_SSL_PROVIDER=package \
-#  ../..
-#sudo make -j 4 install
-#popd
+  mkdir -p "cmake/build"
+  pushd "cmake/build"
+  cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DgRPC_INSTALL=ON \
+  -DgRPC_BUILD_TESTS=OFF \
+  -DgRPC_SSL_PROVIDER=package \
+  ../..
+  sudo make -j 4 install
+  popd
+}
 
+function cross_compile_grpc()
+{
+  local prefix="aarch64-linux-android"
+  export GRPC_CROSS_COMPILE=true
+ # export PATH=/home/ubuntu/stand-alone-toolchain/arm-26-toolchain-clang/bin:$PATH
+  export SYSROOT=$TOOL_HOME/sysroot
+  export HOST_CC="/usr/bin/gcc"
+  export HOST_CXX="/usr/bin/g++"
+  export HOST_LD="/usr/bin/ld"
+  export CC="${prefix}-clang --sysroot $SYSROOT"
+  export CXX="${prefix}-clang++ --sysroot $SYSROOT"
+  export LD="${prefix}-clang++"
+  export LDXX="${prefix}-clang++" 
+  export AR="${prefix}-ar"
+  export STRIP="${prefix}-strip"
+  export PROTOBUF_CONFIG_OPTS="--host=${prefix} --with-sysroot=${SYSROOT} --with-protoc=/usr/local/bin/protoc CFLAGS='-march=armv7-a -D__ANDROID_API__=26' CXXFLAGS='-frtti -fexceptions -march=armv7-a -D__ANDROID_API__=26' LIBS='-llog -lz -lc++_static'"
+  export HAS_PKG_CONFIG=false
+  export GRPC_CROSS_LDOPTS="-L$SYSROOT -L$GRPC"
+  export GRPC_CROSS_AROPTS="rc --target=elf64-little"
+  
+  make -j 4
+}
 
-#STAGE=/tmp/aarch64_stage
-STAGE=$RESULT/aarch64_stage
-TOOLCHAIN_CMAKE=/tmp/toolchain.cmake
-# SYSROOT=$($CC -print-sysroot)
-SYSROOT=$TOOL_HOME/sysroot
-CC="aarch64-linux-android-clang"
-CXX="aarch64-linux-android-clang++"
+function cmake_cross_compile_grpc()
+{
+   #STAGE=/tmp/aarch64_stage
+   STAGE=$RESULT/aarch64_stage
+   TOOLCHAIN_CMAKE=/tmp/toolchain.cmake
+   # SYSROOT=$($CC -print-sysroot)
+   SYSROOT=$TOOL_HOME/sysroot
+   CC="aarch64-linux-android-clang"
+   CXX="aarch64-linux-android-clang++"
 
 # sudo ln -sf $SYSROOT/usr/lib/libc.so  $SYSROOT/usr/lib/libpthread.so
 # sudo ln -sf $SYSROOT/usr/lib/libc.so  $SYSROOT/usr/lib/librt.so
@@ -79,16 +119,22 @@ make -j 4 install
 popd
 
 # -D__ANDROID_API__=21 \
+}
 
-mkdir -p "examples/cpp/helloworld/cmake/aarch64_build"
-pushd "examples/cpp/helloworld/cmake/aarch64_build"
-cmake -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_CMAKE \
+function build_hello_world()
+{
+   mkdir -p "examples/cpp/helloworld/cmake/aarch64_build"
+   pushd "examples/cpp/helloworld/cmake/aarch64_build"
+   cmake -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_CMAKE \
       -DCMAKE_BUILD_TYPE=Release \
       -DProtobuf_DIR=$STAGE/lib/cmake/protobuf \
       -DgRPC_DIR=$STAGE/lib/cmake/grpc \
       -DCMAKE_INSTALL_PREFIX=$RESULT \
       ../..
-make -j 4
-popd
+   make -j 4
+   popd
 
-mv examples/cpp/helloworld/cmake/aarch64_build $RESULT
+   mv examples/cpp/helloworld/cmake/aarch64_build $RESULT
+}
+
+app $*
